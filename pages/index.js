ec5a70012b969e31db58fedac084a8dd95960077
alitemel89 <alitemel89.ce@gmail.com>
2021-12-15 15:05:19 +0300

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import { PostCard, PostWidget, Categories } from "../components";
 import { getPosts } from "../services";
 
 export default function Home({ posts }) {
-
   console.log(posts);
   return (
     <div className="container mx-auto px-10 mb-8 bg-gray-300">
@@ -17,11 +16,12 @@ export default function Home({ posts }) {
             <PostCard post={post.node} key={post.title} />
           ))}
         </div>
-      </div>
-      <div className="lg:col-span-4 col-span-1">
-        <div className="lg:sticky relative top-8">
-          <PostWidget />
-          <Categories />
+
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
         </div>
       </div>
     </div>
