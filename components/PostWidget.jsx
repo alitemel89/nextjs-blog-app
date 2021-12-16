13d9f1a5104584bc52b1e9c5f25c352e67e2fc31
alitemel89 <alitemel89.ce@gmail.com>
2021-12-16 19:50:49 +0300

    add categories component

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -19,13 +19,13 @@ const PostWidget = ({ categories, slug }) => {
   console.log(relatedPosts);
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
-      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+      <h3 className="text-xl mb-4 font-semibold border-b pb-4">
         {slug ? "Related Posts" : "Recent Posts"}
       </h3>
       {relatedPosts.map((post, index) => (
         <div key={index}>
-          <div>
-            <p>{moment(post.createdAt).format("MMM DD, YYYY")}</p>
+          <div className="flex-grow ml-2 my-3">
+            <p className="text-gray-400 font-xs">{moment(post.createdAt).format("MMM DD, YYYY")}</p>
             <Link href={`/post/${post.slug}`} className="text-md" key={index}>
               {post.title}
             </Link>
--- services/index.js
+++ services/index.js
@@ -73,3 +73,19 @@ export const getSimilarPosts = async () => {
     }
   `;
 };
+
+
+export const getCategories = async () => {
+  const query = gql`
+    query GetCategories {
+        categories {
+          name
+          slug
+        }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.categories;
+};
